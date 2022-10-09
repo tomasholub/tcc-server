1882942ede83856f504f821299a7b30fa99ef8ef
Tomas Holub <tomas.holub@puppet.com>
2022-10-09 13:59:29 +0200

    Port 80 is used by default (on render.com always).

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 80;
 
 app.get('/', (req, res) => res.json({ number: 1 }));
 
